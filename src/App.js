daca6feca1cb10a86786a9bfb0197ab0eb414fec
Misael Reyes <misaelrey90@gmail.com>
2023-09-13 16:22:15 -0500

    redesigned service section with animation and modals

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import logo from "./logo.svg";
 import "./App.css";
+import {animate, motion, AnimatePresence} from "framer-motion";
+import {useState} from "react";
 
 function App() {
 	return (
@@ -7,8 +9,8 @@ function App() {
 			<nav className="navbar navbar-expand-lg navbar-dark bg-dark nav position-static">
 				<div className="col-md col align-self-center">
 					<img
-						src={logo}
-						className=" float-start"
+						src="./img/1Asset.png"
+						className=" float-start mx-4"
 						height="50"
 						alt="logo"
 					/>
@@ -46,13 +48,12 @@ function App() {
 							CALL
 						</button>
 					</div>
-					
 				</div>
 
 				<div className="px-4">
-						<button className="btn btn-outline-info d-lg-none d-flex">
-							CALL (972)946-5331
-						</button>
+					<button className="btn btn-outline-info d-lg-none d-flex">
+						CALL (972)946-5331
+					</button>
 				</div>
 				<div className="col d-lg-none d-flex justify-content-end">
 					<div className="dropdown px-3">
@@ -103,42 +104,373 @@ function App() {
 					<p className="text-white fs-4">
 						We are the best in the business
 					</p>
-					<a className="btn btn-info text-dark fw-medium fs-3 m-3 " href="https://calendly.com/dallaspowerwashpros/free-quote-evaluation" target="blank">
+					<a
+						className="btn btn-info text-dark fw-medium fs-3 m-3 "
+						href="https://calendly.com/dallaspowerwashpros/free-quote-evaluation"
+						target="blank"
+					>
 						Book Now
 					</a>
-					<button className="btn btn-info text-dark fw-medium fs-3 m-3" href="tel:9729465331">
+					<button
+						className="btn btn-info text-dark fw-medium fs-3 m-3"
+						href="tel:9729465331"
+					>
 						Call Us
 					</button>
 				</div>
 			</section>
 			<section id="services" className="container">
 				<h1 className="">Our Services</h1>
-				<div className="row p-3">
-					<img src="./img/ServiceDriveway.png" className="img-fluid col-6 col-xl-3 h-100 m-auto" alt="..."></img>
-					<div className="col-xl col-12">
-						<h2 className="text-xl-start text-center">Driveway Cleaning</h2>
-						<p className="fs-5 paragraph">Revitalize your driveway with our professional pressure washing service! At Dallas Powerwash Pros, we specialize in driveway cleaning that leaves your property looking fresh and pristine. We pay meticulous attention to detail, ensuring that every nook and cranny is thoroughly cleaned. With our expertise, your driveway will be transformed into a welcoming space, enhancing the overall aesthetics of your property.</p>
+				<div className="row align-items-end row-cols-1 row-cols-lg-4">
+					<div className="col p-3">
+						<h2 className="text-center">Driveway Cleaning</h2>
+						<img
+							src="./img/Driveway.png"
+							className="img-fluid  m-auto rounded-5"
+							alt="..."
+						></img>
+						<div className="col-xl col-12">
+							<motion.button
+								type="button"
+								className="btn btn-primary rounded-pill m-3 align-top"
+								whileHover={{scale: 1.2}}
+								data-bs-toggle="modal"
+								data-bs-target="#DrivewayServiceModal"
+							>
+								more
+							</motion.button>
+							<motion.div
+								class="modal fade"
+								id="DrivewayServiceModal"
+								tabindex="-1"
+								aria-labelledby="DrivewayServiceModalLabel"
+								aria-hidden="true"
+								initial={{scale:.3}}
+								whileInView={{scale: 1 }}
+								transition={{ type: "spring", damping: 13, bounce: 1, duration: 1 }}
+							>
+								<div class="modal-dialog">
+									<div class="modal-content">
+										<div class="modal-header">
+											<h1
+												class="modal-title fs-5"
+												id="DrivewayServiceModalLabel"
+											>
+												Driveway Cleaning
+											</h1>
+											<button
+												type="button"
+												class="btn-close"
+												data-bs-dismiss="modal"
+												aria-label="Close"
+											></button>
+										</div>
+										<div class="modal-body">
+											<p className="fs-5 paragraph">
+												Revitalize your driveway with
+												our professional pressure
+												washing service! At Dallas
+												Powerwash Pros, we specialize in
+												driveway cleaning that leaves
+												your property looking fresh and
+												pristine. We pay meticulous
+												attention to detail, ensuring
+												that every nook and cranny is
+												thoroughly cleaned. With our
+												expertise, your driveway will be
+												transformed into a welcoming
+												space, enhancing the overall
+												aesthetics of your property.
+											</p>
+										</div>
+										<div class="modal-footer">
+											<button
+												type="button"
+												class="btn btn-secondary"
+												data-bs-dismiss="modal"
+											>
+												Close
+											</button>
+											<a
+												class="btn btn-primary"
+												href="https://calendly.com/dallaspowerwashpros/free-quote-evaluation"
+												target="blank"
+											>
+												Book Service
+											</a>
+										</div>
+									</div>
+								</div>
+							</motion.div>
+						</div>
 					</div>
-				</div>
-				<div className="row p-3">
-					<img src="./img/ServiceSiding.png" className="img-fluid col-6 col-xl-3 h-100 m-auto" alt="..."></img>
-					<div className="col-xl col-12">
-						<h2 className="text-xl-start text-center">Siding Cleaning</h2>
-						<p className="fs-5 paragraph">Revive the beauty of your home's siding with our professional soft washing service at Dallas Powerwash Pros. Over time, siding can accumulate dirt, mildew, algae, and other contaminants. Soft washing is a safe and effective cleaning method that uses low-pressure techniques and environmentally-friendly solutions to gently remove dirt and grime. Whether you have vinyl, wood, stucco, or any other type of siding, we tailor our approach to ensure optimal results. Trust Dallas Powerwash Pros to deliver exceptional results, prompt service, and unmatched customer satisfaction.</p>
+					<div className="col p-3">
+						<h2 className="text-center">Siding Cleaning</h2>
+						<img
+							src="./img/Siding.png"
+							className="img-fluid m-auto rounded-5"
+							alt="..."
+						></img>
+						<div className="col-xl col-12">
+							<motion.button
+								type="button"
+								className="btn btn-primary rounded-pill inline m-3"
+								whileHover={{scale: 1.2}}
+								data-bs-toggle="modal"
+								data-bs-target="#SidingServiceModal"
+							>
+								more
+							</motion.button>
+							<motion.div
+								class="modal fade"
+								id="SidingServiceModal"
+								tabindex="-1"
+								aria-labelledby="SidingServiceModalLabel"
+								aria-hidden="true"
+								initial={{scale:.3}}
+								whileInView={{scale: 1 }}
+								transition={{ type: "spring", damping: 13, bounce: 1, duration: 1 }}
+							>
+								<div class="modal-dialog">
+									<div class="modal-content">
+										<div class="modal-header">
+											<h1
+												class="modal-title fs-5"
+												id="SidingServiceModalLabel"
+											>
+												Siding Cleaning
+											</h1>
+											<button
+												type="button"
+												class="btn-close"
+												data-bs-dismiss="modal"
+												aria-label="Close"
+											></button>
+										</div>
+										<div class="modal-body">
+											<p className="fs-5 paragraph">
+												Revive the beauty of your home's
+												siding with our professional
+												soft washing service at Dallas
+												Powerwash Pros. Over time,
+												siding can accumulate dirt,
+												mildew, algae, and other
+												contaminants. Soft washing is a
+												safe and effective cleaning
+												method that uses low-pressure
+												techniques and
+												environmentally-friendly
+												solutions to gently remove dirt
+												and grime. Whether you have
+												vinyl, wood, stucco, or any
+												other type of siding, we tailor
+												our approach to ensure optimal
+												results. Trust Dallas Powerwash
+												Pros to deliver exceptional
+												results, prompt service, and
+												unmatched customer satisfaction.
+											</p>
+										</div>
+										<div class="modal-footer">
+											<button
+												type="button"
+												class="btn btn-secondary"
+												data-bs-dismiss="modal"
+											>
+												Close
+											</button>
+											<a
+												class="btn btn-primary"
+												href="https://calendly.com/dallaspowerwashpros/free-quote-evaluation"
+												target="blank"
+											>
+												Book Service
+											</a>
+										</div>
+									</div>
+								</div>
+							</motion.div>
+						</div>
 					</div>
-				</div>
-				<div className="row p-3">	
-					<img src="./img/ServiceRoof.png" className="img-fluid col-6 col-xl-3 h-100 m-auto" alt="..."></img>
-					<div className="col-xl col-12">
-						<h2 className="text-xl-start text-center">Roof Cleaning</h2>
-						<p className="fs-5 paragraph">Transform the appearance of your roof with our expert roof cleaning services at Dallas Powerwash Pros. Over time, roofs can become plagued by unsightly stains, moss, algae, and debris, which not only diminish their visual appeal but also compromise their longevity and structural integrity. Using industry-approved techniques and eco-friendly cleaning solutions, we'll remove stubborn stains, moss, and algae, restoring your roof to its original beauty. Let us revitalize your roof, improve its durability, and enhance the overall aesthetic of your home.</p>
+					<div className="col p-3">
+						<h2 className="text-center">Roof Cleaning</h2>
+						<img
+							src="./img/Roof.png"
+							className="img-fluid m-auto rounded-5"
+							alt="..."
+						></img>
+						<div className="col-xl col-12">
+							<motion.button
+								type="button"
+								className="btn btn-primary rounded-pill m-3"
+								whileHover={{scale: 1.2}}
+								data-bs-toggle="modal"
+								data-bs-target="#RoofServiceModal"
+							>
+								more
+							</motion.button>
+							
+							<motion.div
+								class="modal"
+								id="RoofServiceModal"
+								tabindex="-1"
+								aria-labelledby="RoofServiceModalLabel"
+								aria-hidden="true"
+								initial={{scale:.3}}
+								whileInView={{scale: 1 }}
+								transition={{ type: "spring", damping: 13, bounce: 1, duration: 1 }}
+							>
+								<div class="modal-dialog">
+									<div class="modal-content">
+										<div class="modal-header">
+											<h1
+												class="modal-title fs-5"
+												id="RoofServiceModalLabel"
+											>
+												Roof Cleaning
+											</h1>
+											<button
+												type="button"
+												class="btn-close"
+												data-bs-dismiss="modal"
+												aria-label="Close"
+											></button>
+										</div>
+										<div class="modal-body">
+											<p className="fs-5 paragraph">
+												Transform the appearance of your
+												roof with our expert roof
+												cleaning services at Dallas
+												Powerwash Pros. Over time, roofs
+												can become plagued by unsightly
+												stains, moss, algae, and debris,
+												which not only diminish their
+												visual appeal but also
+												compromise their longevity and
+												structural integrity. Using
+												industry-approved techniques and
+												eco-friendly cleaning solutions,
+												we'll remove stubborn stains,
+												moss, and algae, restoring your
+												roof to its original beauty. Let
+												us revitalize your roof, improve
+												its durability, and enhance the
+												overall aesthetic of your home.
+											</p>
+										</div>
+										<div class="modal-footer">
+											<button
+												type="button"
+												class="btn btn-secondary"
+												data-bs-dismiss="modal"
+											>
+												Close
+											</button>
+											<a
+												class="btn btn-primary"
+												href="https://calendly.com/dallaspowerwashpros/free-quote-evaluation"
+												target="blank"
+											>
+												Book Service
+											</a>
+										</div>
+									</div>
+								</div>
+							</motion.div>
+							
+						</div>
 					</div>
-				</div>
-				<div className="row p-3">
-					<img src="./img/ServiceDecks.png" className="img-fluid col-6 col-xl-3 h-100 m-auto" alt="..."></img>
-					<div className="col-xl col-12">
-						<h2 className="text-xl-start text-center">Deck Cleaning & Staining</h2>
-						<p className="fs-5 lg paragraph">Transform your weathered wood deck into a stunning outdoor oasis with our comprehensive deck cleaning and staining services. At Dallas Powerwash Pros, we specialize in reviving and protecting wooden decks, ensuring they stand the test of time while maintaining their natural beauty. We pay meticulous attention to detail, ensuring every inch of your deck is rejuvenated. Once the deck is clean and dry, we expertly apply high-quality wood stains or sealants, carefully chosen to enhance the natural grain and color of your deck while providing superior protection against the elements. Our goal is to create a deck that not only looks stunning but also withstands the rigors of outdoor conditions for years to come. </p>
+					<div className="col p-3">
+						<h2 className="text-center">Deck & Fence Staining</h2>
+						<img
+							src="./img/Deck.png"
+							className="img-fluid m-auto rounded-5"
+							alt="..."
+						></img>
+						<div className="col-xl col-12">
+							<motion.button
+								type="button"
+								className="btn btn-primary rounded-pill m-3"
+								whileHover={{scale: 1.2}}
+								data-bs-toggle="modal"
+								data-bs-target="#DeckServiceModal"
+							>
+								more
+							</motion.button>
+							<motion.div
+								class="modal"
+								id="DeckServiceModal"
+								tabindex="-1"
+								aria-labelledby="DeckServiceModalLabel"
+								aria-hidden="true"
+								initial={{scale:.3}}
+								whileInView={{scale: 1 }}
+								transition={{ type: "spring", damping: 13, bounce: 1, duration: 1 }}
+							>
+								<div class="modal-dialog">
+									<div class="modal-content">
+										<div class="modal-header">
+											<h1
+												class="modal-title fs-5"
+												id="DeckServiceModalLabel"
+											>
+												Deck & Fence Staining
+											</h1>
+											<button
+												type="button"
+												class="btn-close"
+												data-bs-dismiss="modal"
+												aria-label="Close"
+											></button>
+										</div>
+										<div class="modal-body">
+											<p className="fs-5 lg paragraph">
+												Transform your weathered wood
+												deck into a stunning outdoor
+												oasis with our comprehensive
+												deck cleaning and staining
+												services. At Dallas Powerwash
+												Pros, we specialize in reviving
+												and protecting wooden decks,
+												ensuring they stand the test of
+												time while maintaining their
+												natural beauty. We pay
+												meticulous attention to detail,
+												ensuring every inch of your deck
+												is rejuvenated. Once the deck is
+												clean and dry, we expertly apply
+												high-quality wood stains or
+												sealants, carefully chosen to
+												enhance the natural grain and
+												color of your deck while
+												providing superior protection
+												against the elements. Our goal
+												is to create a deck that not
+												only looks stunning but also
+												withstands the rigors of outdoor
+												conditions for years to come.{" "}
+											</p>
+										</div>
+										<div class="modal-footer">
+											<button
+												type="button"
+												class="btn btn-secondary"
+												data-bs-dismiss="modal"
+											>
+												Close
+											</button>
+											<a
+												class="btn btn-primary"
+												href="https://calendly.com/dallaspowerwashpros/free-quote-evaluation"
+												target="blank"
+											>
+												Book Service
+											</a>
+										</div>
+									</div>
+								</div>
+							</motion.div>
+						</div>
 					</div>
 				</div>
 			</section>
